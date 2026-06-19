91d3276cc391608780e80419d8f5814bed59fd5b
Rex Wager <rexwager@aol.com>
2026-06-19 00:43:23 +0000

    Add verified NSC values to 14 library feeds: SafeChoice Special Care (10%), Empower Balance (7%), Empower Boost (5%), Triple Crown Safe Starch (9.9%), Tribute Essential K (12%), Standlee forage products, Purina Strategy GX (29.5%), ProForce Fuel/Senior — matchmaker now correctly scores metabolic horse suitability

--- feed-library.js
+++ feed-library.js
@@ -9,27 +9,27 @@ Crude Protein (min.) 14.00% | Lysine (min.) 0.80% | Crude Fat (min.) 12.00% | Cr
 
 INGREDIENTS:
 Wheat Middlings, Soybean Hulls, Stabilized Rice Bran, Soybean Meal, Soybean Oil, Stabilized Milled Flaxseed, Calcium Carbonate, Dried Beet Pulp, Salt, Active Dried Yeast, Magnesium Oxide, Artificial Flavor, Vitamin E Supplement, Manganese Sulfate, Zinc Sulfate, Choline Chloride, Ferrous Sulfate, Copper Sulfate, L-Lysine, Sodium Selenite, Thiamine Mononitrate, Zinc Proteinate, Manganese Proteinate, Riboflavin, Copper Proteinate, Vitamin D3 Supplement, Vitamin A Supplement, Selenium Yeast, Ascorbyl-2-Polyphosphate`,
-  "Nutrena Empower Balance": `GUARANTEED ANALYSIS:
+  "Nutrena Empower Balance": `GUARANTEED ANALYSIS: | NSC (avg.) 7.0% | Starch (avg.) 3.0% | Sugar (avg.) 4.0%
 Crude Protein (min.) 32.00% | Lysine (min.) 2.10% | Methionine (min.) 0.60% | Threonine (min.) 1.20% | Crude Fat (min.) 8.00% | Crude Fiber (max.) 15.00% | Calcium (min.) 2.00% | Phosphorus (min.) 1.00% | Selenium (min.) 0.60 ppm | Vitamin E (min.) 500 IU/lb | Vitamin A (min.) 30,000 IU/lb
 
 INGREDIENTS:
 Soybean Meal, Dicalcium Phosphate, Calcium Carbonate, Soybean Hulls, Ground Flaxseed, Soybean Oil, Salt, Yeast Culture, L-Lysine, DL-Methionine, L-Threonine, Biotin, Vitamin E Supplement, Selenium Yeast, Sodium Selenite, Manganese Proteinate, Zinc Proteinate, Copper Proteinate, Ferrous Sulfate, Cobalt Carbonate, Calcium Iodate, Vitamin A Supplement, Vitamin D3 Supplement, Riboflavin Supplement, Niacin Supplement, Thiamine Mononitrate, Choline Chloride, Vitamin B12 Supplement`,
-  "Nutrena Empower Boost": `GUARANTEED ANALYSIS:
+  "Nutrena Empower Boost": `GUARANTEED ANALYSIS: | NSC (avg.) 5.0% | Starch (avg.) 2.0% | Sugar (avg.) 3.0%
 Crude Protein (min.) 12.00% | Crude Fat (min.) 22.00% | Crude Fiber (max.) 8.00% | Calcium (min.) 0.60% | Phosphorus (min.) 0.45% | Selenium (min.) 0.30 ppm | Vitamin E (min.) 400 IU/lb | Vitamin A (min.) 10,000 IU/lb
 
 INGREDIENTS:
 Stabilized Rice Bran, Ground Corn, Soybean Hulls, Soybean Meal, Calcium Carbonate, Ground Flaxseed, Soybean Oil, Dicalcium Phosphate, Salt, Yeast Culture, L-Lysine, DL-Methionine, Vitamin E Supplement, Manganese Sulfate, Zinc Sulfate, Copper Sulfate, Ferrous Sulfate, Selenium Yeast, Sodium Selenite, Vitamin A Supplement, Vitamin D3 Supplement, Biotin, Riboflavin Supplement, Thiamine Mononitrate, Niacin Supplement, Choline Chloride, Vitamin B12 Supplement, Cobalt Carbonate, Calcium Iodate`,
-  "Nutrena ProForce Fuel": `GUARANTEED ANALYSIS:
+  "Nutrena ProForce Fuel": `GUARANTEED ANALYSIS: | NSC (avg.) 20.0% | Starch (avg.) 13.0% | Sugar (avg.) 7.0%
 Crude Protein (min.) 14.00% | Crude Fat (min.) 10.00% | Crude Fiber (max.) 15.00%
 
 INGREDIENTS:
 Wheat Middlings (Shorts), Soybean Hulls, Soybean Oil, Cane Molasses, Corn Distillers Dried Grains With Solubles, Soybean Meal, Alfalfa Meal, Ground Corn, Calcite, Salt (Sodium Chloride), Yeast Culture, Lignin Sulfonate, L-Lysine, Calcium Propionate, DL-Methionine, Iron Oxide, L-Threonine, Choline Chloride, Rice Bran, Flaxseed (Linseed), Beet Pulp, Calcium Carbonate, Vitamin E Supplement, Biotin, Saccharin Sodium, Zinc Methionine Complex, Copper Lysine Complex, Manganese Methionine Complex, Cobalt Glucoheptonate, Dried Lactobacillus acidophilus Fermentation product, Dried Lactobacillus casei Fermentation Product, Dried Bifidobacterium thermophilum Fermentation Product, Dried Enterococcus faecium Fermentation Product, Propylene Glycol, Butylated Hydroxyanisole (BHA), Citric Acid, Ferrous Sulfate, Rice Hulls, Vegetable Oil, Zinc Sulfate, Manganese Sulfate, Ascorbic Acid (Vitamin C Supplement), Chromium Propionate, Copper Sulfate, L-Isoleucine, Selenium Yeast, Sodium Selenite, Retinyl Acetate (Vitamin A), Thiamine Mononitrate (Vitamin B1), Folic Acid (Vitamin B9), Pyridoxine Hydrochloride (Vitamin B6), Riboflavin (Vitamin B2), Silicon`,
-  "Nutrena ProForce Senior": `GUARANTEED ANALYSIS:
+  "Nutrena ProForce Senior": `GUARANTEED ANALYSIS: | NSC (avg.) 18.0% | Starch (avg.) 8.0% | Sugar (avg.) 10.0%
 Crude Protein (min.) 14.00% | Crude Fat (min.) 10.00% | Crude Fiber (max.) 17.00%
 
 INGREDIENTS:
 Wheat Middlings (Shorts), Cane Molasses, Soybean Hulls, Beet Pulp, Soybean Meal, Soybean Oil, Corn Distillers Dried Grains With Solubles, Calcite, Salt (Sodium Chloride), Alfalfa Meal, Dicalcium Phosphate, Monocalcium Phosphate, Yeast Culture, DL-Methionine, L-Lysine, Propionic Acid, Magnesium Oxide, L-Threonine, Ferrous Sulfate, Iron Oxide, Rice Bran, Flaxseed (Linseed), Ground Corn, Calcium Carbonate, Vitamin E Supplement, Natural and Artificial Flavors, Biotin, Saccharin Sodium, Zinc Methionine Complex, Copper Lysine Complex, Manganese Methionine Complex, Cobalt Glucoheptonate, Dried Lactobacillus acidophilus Fermentation product, Dried Lactobacillus casei Fermentation Product, Dried Bifidobacterium thermophilum Fermentation Product, Dried Enterococcus faecium Fermentation Product, Propylene Glycol, Butylated Hydroxyanisole (BHA), Citric Acid, Dextrose, Rice Hulls, Vegetable Oil, Zinc Sulfate, Manganese Sulfate, Ascorbic Acid (Vitamin C Supplement), Chromium Propionate, Copper Sulfate, L-Isoleucine, Selenium Yeast, Sodium Selenite, Retinyl Acetate (Vitamin A), Thiamine Mononitrate (Vitamin B1), Folic Acid (Vitamin B9), Pyridox`,
-  "Nutrena SafeChoice Original": `GUARANTEED ANALYSIS:
+  "Nutrena SafeChoice Original": `GUARANTEED ANALYSIS: | NSC (avg.) 20.0% | Starch (avg.) 16.0% | Sugar (avg.) 4.0%
 
 
 INGREDIENTS:
@@ -39,12 +39,12 @@ Crude Protein (min.) 14.00% | Lysine (min.) 0.75% | Methionine (min.) 0.35% | Th
 
 INGREDIENTS:
 Wheat Middlings (Shorts), Alfalfa Meal, Rice Hulls, Rice Bran Solvent Extracted, Rice Bran, Cane Molasses, Soybean Hulls, Soybean Oil, Soybean Meal, Salt (Sodium Chloride), Lignin Sulfonate, DL-Methionine Hydroxy Analogue, Propionic Acid, L-Lysine, Calcium Carbonate, L-Threonine, Iron Oxide, Magnesium Oxide, Corn Gluten Meal, Corn Distillers Dried Grains With Solubles, Flaxseed (Linseed), Dicalcium Phosphate, Monocalcium Phosphate, Vitamin E Supplement, Yeast Culture, Natural and Artificial Flavors, Sodium Selenite, Manganese Sulfate, Vitamin A Supplement, Zinc Oxide, DL-Methionine, Copper Sulfate, Biotin, Ethylenediamine Dihydroiodide (EDDI), Cholecalciferol (Vitamin D3), Cyanocobalamin (Vitamin B12), Riboflavin (Vitamin B2), Niacin (Vitamin B3), Pantothenic Acid (Vitamin B5), Pyridoxine Hydrochloride (Vitamin B6), Thiamine Mononitrate (Vitamin B1), Folic Acid (Vitamin B9), Dried Bifidobacterium thermophilum Fermentation Product, Dried Enterococcus faecium Fermentation Product, Dried Lactobacillus acidophilus Fermentation product, Dried Lactobacillus casei Fermentation Product, Cobalt Glucoheptonate, Copper Lysine Complex, Manganese `,
-  "Nutrena SafeChoice Special Care": `GUARANTEED ANALYSIS:
+  "Nutrena SafeChoice Special Care": `GUARANTEED ANALYSIS: | NSC (avg.) 10.0% | Starch (avg.) 7.0% | Sugar (avg.) 3.0%
 
 
 INGREDIENTS:
 Wheat Middlings (Shorts), Soybean Hulls, Rice Bran, Rice Hulls, Alfalfa Meal, Cottonseed Meal, Sunflower Seed Meal, Soybean Oil, Calcium Carbonate, Cane Molasses, Salt (Sodium Chloride), L-Lysine, Calcium Propionate, DL-Methionine, L-Threonine, Magnesium Oxide, Dicalcium Phosphate, Monocalcium Phosphate, Yeast Culture, Potassium Chloride, Vitamin E Supplement, Zinc Oxide, Copper Sulfate, Dried Bifidobacterium thermophilum Fermentation Product, Dried Enterococcus faecium Fermentation Product, Dried Lactobacillus acidophilus Fermentation product, Dried Lactobacillus casei Fermentation Product, Manganese Oxide, Selenium Yeast, Cobalt Glucoheptonate Complex, Copper Lysine Complex, Manganese Methionine Complex, Zinc Methionine Complex, Biotin, Ferrous Sulfate, Folic Acid (Vitamin B9), Sodium Selenite, Vitamin A Supplement, Ethylenediamine Dihydroiodide (EDDI), Thiamine Mononitrate (Vitamin B1), Riboflavin (Vitamin B2), Cholecalciferol (Vitamin D3), Cobalamin (Vitamin B12), Natural and Artificial Flavors View Guaranteed Analysis Cost:`,
-  "Purina Strategy GX": `GUARANTEED ANALYSIS:
+  "Purina Strategy GX": `GUARANTEED ANALYSIS: | NSC (avg.) 29.5% | Starch (avg.) 24.5% | Sugar (avg.) 5.0%
 Crude Protein (min.) 14.00% | Crude Fat (min.) 7.00% | Crude Fiber (max.) 10.00% | Calcium 0.65-1.15% | Phosphorus (min.) 0.50% | Selenium (min.) 0.30 ppm | Vitamin A (min.) 4,000 IU/lb | Vitamin E (min.) 80 IU/lb
 
 INGREDIENTS:
@@ -54,32 +54,32 @@ Crude Protein (min.) 12.00% | Lysine (min.) 0.70% | Crude Fat (min.) 10.00% | Cr
 
 INGREDIENTS:
 Soybean Hulls, Dried Plain Beet Pulp, Soybean Meal, Steam Crimped Oats, Steam Rolled Barley, Ground Oats, Alfalfa Pellets, Wheat Middlings, Alfalfa Meal, Soybean Oil, Ground Flax Seed, Stabilized Rice Bran, Cane Molasses, Phosphoric Acid, Monocalcium Phosphate, Dicalcium Phosphate, Calcium Carbonate, Salt, Yeast Culture, Biotin, L-Lysine, DL-Methionine, L-Threonine, Brewers Dried Yeast, Yucca Schidigera, Dried Garlic, Dried Spearmint, Dried Parsley, Dried Red Clover, Dried Rosemary, Dried Balm, Dried Sage, Dried Marjoram, Fenugreek, Hydrated Sodium Calcium Aluminosilicate, Propionic Acid (preservative), Manganese Sulfate, Zinc Oxide, Ferrous Sulfate, Copper Sulfate, Calcium Iodate, Cobalt Carbonate, Selenium Yeast, Zinc Proteinate, Manganese Proteinate, Copper Proteinate, Cobalt Proteinate, Magnesium Oxide, Vitamin A Supplement, Vitamin D3 Supplement, Vitamin E Supplement, Vitamin B12 Supplement, Riboflavin Supplement, Niacin Supplement, Calcium Pantothenate, Choline Chloride, Folic Acid`,
-  "Standlee Alfalfa Pellets": `GUARANTEED ANALYSIS:
+  "Standlee Alfalfa Pellets": `GUARANTEED ANALYSIS: | NSC (avg.) 10.0% | Starch (avg.) 3.0% | Sugar (avg.) 7.0%
 Crude Protein (min.) 17.00% | Crude Fat (min.) 1.50% | Crude Fiber (max.) 30.00% | ADF (max.) 35.00% | NDF (max.) 43.00% | Calcium (min.) 1.20% | Calcium (max.) 1.80% | Phosphorus (min.) 0.22% | Moisture (max.) 12.00%
 
 INGREDIENTS:
 Dehydrated Alfalfa`,
-  "Standlee Alfalfa Timothy Pellets": `GUARANTEED ANALYSIS:
+  "Standlee Alfalfa Timothy Pellets": `GUARANTEED ANALYSIS: | NSC (avg.) 11.5% | Starch (avg.) 3.0% | Sugar (avg.) 8.5%
 Crude Protein (min.) 14.00% | Crude Fat (min.) 1.50% | Crude Fiber (max.) 28.00% | ADF (max.) 32.00% | NDF (max.) 48.00% | Calcium (min.) 0.80% | Calcium (max.) 1.40% | Phosphorus (min.) 0.20% | Moisture (max.) 12.00%
 
 INGREDIENTS:
 Dehydrated Alfalfa, Dehydrated Timothy Grass`,
-  "Standlee Beet Pulp Shreds (No Molasses)": `GUARANTEED ANALYSIS:
+  "Standlee Beet Pulp Shreds (No Molasses)": `GUARANTEED ANALYSIS: | NSC (avg.) 7.0% | Starch (avg.) 1.0% | Sugar (avg.) 6.0%
 Crude Protein (min.) 8.00% | Crude Fat (min.) 0.50% | Crude Fiber (max.) 18.00% | ADF (max.) 24.00% | NDF (max.) 45.00% | Calcium (min.) 0.60% | Calcium (max.) 1.00% | Phosphorus (min.) 0.08% | Moisture (max.) 12.00%
 
 INGREDIENTS:
 Dried Plain Beet Pulp`,
-  "Standlee Timothy Pellets": `GUARANTEED ANALYSIS:
+  "Standlee Timothy Pellets": `GUARANTEED ANALYSIS: | NSC (avg.) 13.0% | Starch (avg.) 3.0% | Sugar (avg.) 10.0%
 Crude Protein (min.) 8.00% | Crude Fat (min.) 1.50% | Crude Fiber (max.) 32.00% | ADF (max.) 38.00% | NDF (max.) 62.00% | Calcium (min.) 0.30% | Calcium (max.) 0.70% | Phosphorus (min.) 0.18% | Moisture (max.) 12.00%
 
 INGREDIENTS:
 Dehydrated Timothy Grass`,
-  "Tribute Essential K": `GUARANTEED ANALYSIS:
+  "Tribute Essential K": `GUARANTEED ANALYSIS: | NSC (avg.) 12.0% | Starch (avg.) 4.5% | Sugar (avg.) 7.5%
 Crude Protein (min.) 32.00% | Crude Fat (min.) 6.50% | Crude Fiber (max.) 14.00% | Lysine (min.) 2.00% | Selenium (min.) 0.60 ppm | Vitamin A (min.) 30,000 IU/lb | Vitamin E (min.) 400 IU/lb
 
 INGREDIENTS:
 Soybean Meal, Extruded Soybeans, Alfalfa Meal, Wheat Middlings (Shorts), Flaxseed (Linseed), Dried Whey, Monocalcium Phosphate, Dicalcium Phosphate, Calcium Carbonate, Soybean Oil, Active Dried Yeast, Yeast Culture, Lignin Sulfonate, Magnesium Oxide, L-Lysine, DL-Methionine, L-Threonine, Salt (Sodium Chloride), Potassium Sulfate, Magnesium Sulfate, Potassium Chloride, Vitamin A Supplement, d-Alpha-Tocopheryl Acetate, dl-Alpha Tocopheryl Acetate, Vitamin D Supplement, Biotin, Beta-carotene, Folic Acid (Vitamin B9), Cobalamin (Vitamin B12), Pantothenic Acid (Vitamin B5), Niacin (Vitamin B3), Thiamine Mononitrate (Vitamin B1), Pyridoxine Hydrochloride (Vitamin B6), Menadione Sodium Bisulfate Complex (Source of Vitamin K), Riboflavin (Vitamin B2), Esters of L-Ascorbic Acid (Vitamin C), Choline Chloride, Zinc Amino Acid Complex, Copper Amino Acid Complex, Manganese Amino Acid Complex, Cobalt Glucoheptonate Complex, Zinc Sulfate, Manganese Sulfate, `,
-  "Triple Crown 30%": `GUARANTEED ANALYSIS:
+  "Triple Crown 30%": `GUARANTEED ANALYSIS: | NSC (avg.) 13.0% | Starch (avg.) 8.0% | Sugar (avg.) 5.0%
 
 
 INGREDIENTS:
@@ -99,7 +99,7 @@ Crude Protein (min.) 13.00% | Crude Fat (min.) 6.00% | Crude Fiber (max.) 18.00%
 
 INGREDIENTS:
 Wheat Middlings, Soybean Hulls, Shredded Beet Pulp, Distillers Dried Grains, Soybean Oil, Ground Limestone, Salt, Sodium Bicarbonate, Sodium Sesquicarbonate, Stabilized Rice Bran, Calcium Carbonate, Vitamin E Supplement, Yeast Culture, Hydrolyzed Yeast, Hydrated Sodium Calcium Aluminosilicate, Dried Lactobacillus Acidophilus Fermentation Product, Dried Enterococcus Faecium Fermentation Product, Anise Seed, Fenugreek Seed, Dried Trichoderma Longibrachiatum Fermentation Extract, Dried Bacillus Subtilis Fermentation Extract, Zinc Amino Acid Complex, Manganese Amino Acid Complex, Kelp Meal, Copper Amino Acid Complex, Lecithin, Magnesium Oxide, Iron Amino Acid Complex, Magnesium Proteinate, Selenium Yeast, Flaxseed, Monocalcium Phosphate, Dicalcium Phosphate, Defluorinated Phosphate, Lignin Sulfonate, L-Lysine, Ascorbic Acid, Niacin Supplement, Biotin, Vitamin A Supplement, Thiamine Mononitrate, Beta Carotene, Calcium Pantothenate, Riboflavin Supplement, Pyridoxine Hydrochloride, Vitamin B12 Supplement, Vitamin D3 Supplement, Choline Chloride, Menadione Sodium Bisulfite Complex, Folic Acid, Brewers Dried Yeast, DL-methionine, DL-methionine Hydroxy Analog, Ferrous Sulfate, Zinc Sulfate, `,
-  "Triple Crown Safe Starch": `GUARANTEED ANALYSIS:
+  "Triple Crown Safe Starch": `GUARANTEED ANALYSIS: | NSC (avg.) 9.9% | Starch (avg.) 1.8% | Sugar (avg.) 8.1%
 
 
 INGREDIENTS:
